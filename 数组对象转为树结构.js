4f95af820c7777e8e324f57dda15e779841f902a
zhuanghongbin <2122689929@qq.com>
2022-12-01 18:55:20 +0800

    first commit json数组转树结构

---
+++ 数组对象转为树结构.js
@@ -0,0 +1,113 @@
+/**
+ * 数组转为树结构
+ * 两个循环, 浅拷贝特性, 返回result
+ * @param list 源数组
+ */
+const listToTree = (list) => {
+  // 定义最终返回的数据结构
+  const result = []
+
+  // 用一个 map 定义数组的每一项, key为listItem的id, value为listItem
+  // 每个 mapItem 加个children字段, pid不为-1 (有pidItem)
+  // 就往当前item对应的pidItem的children里塞当前的mapItem
+  // 否则就往result中塞当前的mapItem
+  const listMap = {}
+
+  // 循环一次, 将listMap填充
+  for (let item of list) {
+    listMap[item.id] = {
+      ...item,
+      children: []
+    }
+  }
+
+  // 再循环一次, 判断, 将list转tree
+  list.forEach(item => {
+    const id = item.id
+    const pid = item.pid
+    // 父节点
+    const pidItem = pid === -1 ? null : listMap[pid]
+    // 当前的mapItem
+    const mapItem = listMap[id]
+    // 进行 push 操作
+    if (pidItem) {
+      pidItem.children.push({ ...mapItem })
+    } else {
+      result.push({ ...mapItem })
+    }
+  })
+  return result
+}
+
+const data = [
+  {
+    pid: '-1',
+    id: '1'
+  },
+  {
+    pid: '-1',
+    id: '2'
+  },
+  {
+    pid: '2',
+    id: '2-2'
+  },
+  {
+    pid: '1',
+    id: '1-1'
+  },
+  {
+    pid: '1-1',
+    id: '1-1-1'
+  },
+  {
+    pid: '2',
+    id: '2-1'
+  },
+  {
+    pid: '1-1',
+    id: '1-1-2'
+  },
+]
+
+listToTree(data)
+
+
+/** 结果对照 */
+const target = [
+  {
+    pid: -1,
+    id: 1,
+    children: [
+      {
+        pid: 1,
+        id: 1 - 1,
+        children: [
+          {
+            pid: 1 - 1,
+            id: 1 - 1 - 1
+          },
+          {
+            pid: 1 - 1,
+            id: 1 - 1 - 2
+          }
+        ]
+      }
+    ]
+  },
+  {
+    pid: -1,
+    id: 2,
+    children: [
+      {
+        pid: 2,
+        id: 2 - 1
+      },
+      {
+        pid: 2,
+        id: 2 - 2
+      }
+    ]
+  }
+]
+
